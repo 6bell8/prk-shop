cf92a61d8288bf722becf1ddb28b8fb5ccca1a32
par jin sung <parkgutime@gmail.com>
2022-10-11 17:17:47 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
 
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 import "./App.css";
 import "./css/layout.css";
+import bg from "./img/shoe.png";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -18,7 +23,42 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+      <div className="container">
+        <div className="row">
+          {/* <div className="col-md-4">
+            <img
+              src={process.env.PUBLIC_URL + "/subBg01.jpg"}
+              width="80%"
+              className="subBg01"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + "/subBg02.jpg"} width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + "/subBg03.jpg"} width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div> */}
+          {shoes.map(function (a, i) {
+            return (
+              <div className="col-md-4" key={i}>
+                <img src={process.env.PUBLIC_URL + shoes[i].img} width="80%" />
+                <h4>{shoes[i].title}</h4>
+                <p>{shoes[i].price}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,30 @@
+// let b = { name: "park", age: 20 };
+// b.name;
+
+let data = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: 120000,
+    img: "/subBg01.jpg",
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: 110000,
+    img: "/subBg02.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    price: 130000,
+    img: "/subBg03.jpg",
+  },
+];
+
+export default data;
