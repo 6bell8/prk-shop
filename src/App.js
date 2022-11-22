747201d1c4ec85986d515d10eae59ead7556432b
par jin sung <parkgutime@gmail.com>
2022-11-22 15:28:06 +0900

    리덕스

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 /* eslint-disable */
 
 import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect, createContext, lazy, Suspense } from "react";
 import "./App.css";
 import "./css/layout.css";
 import bg from "./img/shoe.png";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-import Detail from "./routes/Detail.js";
+
 import Loading from "./routes/Loading";
 import axios from "axios";
-import Cart from "./routes/Cart";
 import { useQuery } from "react-query";
+
+const Detail = lazy(() => import("./routes/Detail"));
+const Cart = lazy(() => import("./routes/Cart"));
+// import Detail from "./routes/Detail.js";
+// import Cart from "./routes/Cart";
 
 // export let Context1 = createContext(); // state보관함
 
@@ -96,7 +100,7 @@ function App() {
             </Nav.Link>
             <Nav.Link
               onClick={() => {
-                navigate("/detail/1");
+                navigate("/detail/0");
               }}
             >
               Detail
@@ -125,97 +129,102 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div
-                className="main-bg"
-                style={{ backgroundImage: "url(" + bg + ")" }}
-              ></div>
-              <div className="cartBox">
-                <p style={{ margin: "10px" }}>최근 본 상품</p>
-
-                {꺼낸거 !== null
-                  ? 꺼낸거.map((a, i) => {
-                      return (
-                        <div className="watchedItem" key={i}>
-                          <img src={`${꺼낸거[i]}`} className="localImg"></img>
-                          <p className="name">{꺼낸거2[i]}</p>
-                        </div>
-                      );
-                    })
-                  : null}
-              </div>
-              <div className="container02">
-                <div className="row">
-                  {shoes.map((a, i) => {
-                    return <Card key={i} shoes={shoes[i]}></Card>;
-                  })}
-                </div>
-              </div>
-
-              {loading == true ? <Loading></Loading> : null}
-
-              <button
-                onClick={() => [
-                  //로딩 중 UI  띄우기
-                  count == 0
-                    ? axios
-                        .get(
-                          "https://gist.githubusercontent.com/6bell8/b54495cd5ef113c5e5048ca64876ee23/raw/a3659fd582e4ede9ee4c7b6482de324915bacf82/shoesData.json"
-                        )
-
-                        .then((result) => {
-                          //가져온 데이터를 shoes라는 데이터에 추가해주세요
-                          // []에서 알맹이만 벗겨서 {} 형태로 남김 ... 괄호를 벗겨주는 문법
-
-                          let copy = [...shoes, ...result.data];
-                          setShoes(copy);
-                          setCount(count + 1);
-                        })
-                        .catch(() => {
-                          console.log("실패했어용");
-                          setLoading(false);
-                          // catch는 if else의 문법과 비슷함
-                          //로딩 중 UI  띄우기
-                        })
-                    : null,
-
-                  // Promise.all([axios.get("/url1"), axios.get("/url2")]).then(
-                  //   () => {
-                  //     //이후 결과값을 알려주는 기능
-                  //   }
-                  // ),
-                ]}
-                className="plusBtn"
-              >
-                {/* useEffect로 1번 이상 클릭하면 button display none 처리하기 */}
-                {count == 0 ? <div>상품 더 보기</div> : <p>마지막 페이지</p>}
-              </button>
-            </>
-          }
-        />
-        {/* //shoes를 data.js에서 받아오지않고 json 형식으로 서비에서 받아오기때문에 도메인이 안뜸 */}
-        {/* Context1 이라는 보관함으로 state를 보관할 대상을 element안에서 묶어준다. */}
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-        <Route path="/cart" element={<Cart />} />
-        {/* nested 문법 핵심은 Route 괄호를 열어서 구성, path 앞부분은 제외 */}
-        <Route path="/about" element={<About />}>
-          <Route path="member" element={<div>멤버이름</div>} />
-          <Route path="location" element={<div>위치정보</div>} />
-        </Route>
-        <Route path="/event" element={<Event />}>
+      {/* // detail 컴포넌트가 로딩 되기 전까지 보여주는 내용 */}
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
           <Route
-            path="one"
-            element={<div>첫 주문 시 적립금 5000원</div>}
-          ></Route>
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
-        </Route>
-        <Route path="*" element={<div>없는 페이지입니다.</div>} />
-      </Routes>
+            path="/"
+            element={
+              <>
+                <div
+                  className="main-bg"
+                  style={{ backgroundImage: "url(" + bg + ")" }}
+                ></div>
+                <div className="cartBox">
+                  <p style={{ margin: "10px" }}>최근 본 상품</p>
+
+                  {꺼낸거 !== null
+                    ? 꺼낸거.map((a, i) => {
+                        return (
+                          <div className="watchedItem" key={i}>
+                            <img
+                              src={`${꺼낸거[i]}`}
+                              className="localImg"
+                            ></img>
+                            <p className="name">{꺼낸거2[i]}</p>
+                          </div>
+                        );
+                      })
+                    : null}
+                </div>
+                <div className="container02">
+                  <div className="row">
+                    {shoes.map((a, i) => {
+                      return <Card key={i} shoes={shoes[i]}></Card>;
+                    })}
+                  </div>
+                </div>
+
+                {loading == true ? <Loading></Loading> : null}
+
+                <button
+                  onClick={() => [
+                    //로딩 중 UI  띄우기
+                    count == 0
+                      ? axios
+                          .get(
+                            "https://gist.githubusercontent.com/6bell8/b54495cd5ef113c5e5048ca64876ee23/raw/a3659fd582e4ede9ee4c7b6482de324915bacf82/shoesData.json"
+                          )
+
+                          .then((result) => {
+                            //가져온 데이터를 shoes라는 데이터에 추가해주세요
+                            // []에서 알맹이만 벗겨서 {} 형태로 남김 ... 괄호를 벗겨주는 문법
+
+                            let copy = [...shoes, ...result.data];
+                            setShoes(copy);
+                            setCount(count + 1);
+                          })
+                          .catch(() => {
+                            console.log("실패했어용");
+                            setLoading(false);
+                            // catch는 if else의 문법과 비슷함
+                            //로딩 중 UI  띄우기
+                          })
+                      : null,
+
+                    // Promise.all([axios.get("/url1"), axios.get("/url2")]).then(
+                    //   () => {
+                    //     //이후 결과값을 알려주는 기능
+                    //   }
+                    // ),
+                  ]}
+                  className="plusBtn"
+                >
+                  {/* useEffect로 1번 이상 클릭하면 button display none 처리하기 */}
+                  {count == 0 ? <div>상품 더 보기</div> : <p>마지막 페이지</p>}
+                </button>
+              </>
+            }
+          />
+          {/* //shoes를 data.js에서 받아오지않고 json 형식으로 서비에서 받아오기때문에 도메인이 안뜸 */}
+          {/* Context1 이라는 보관함으로 state를 보관할 대상을 element안에서 묶어준다. */}
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+          <Route path="/cart" element={<Cart />} />
+          {/* nested 문법 핵심은 Route 괄호를 열어서 구성, path 앞부분은 제외 */}
+          <Route path="/about" element={<About />}>
+            <Route path="member" element={<div>멤버이름</div>} />
+            <Route path="location" element={<div>위치정보</div>} />
+          </Route>
+          <Route path="/event" element={<Event />}>
+            <Route
+              path="one"
+              element={<div>첫 주문 시 적립금 5000원</div>}
+            ></Route>
+            <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
+          </Route>
+          <Route path="*" element={<div>없는 페이지입니다.</div>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
